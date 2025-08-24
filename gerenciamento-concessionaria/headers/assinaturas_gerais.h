aed667af1ef6c172fb9277ef1ce76a3a13174070
Davi Abner <davi.abner57@gmail.com>
2025-08-24 17:35:16 -0300

    feat: adicionar arquivo de cabeçalho assinaturas_gerais.h

---
+++ gerenciamento-concessionaria/headers/assinaturas_gerais.h
@@ -0,0 +1,26 @@
+#ifndef ASSINATURASGERAIS_H_INCLUDED
+#define ASSINATURASGERAIS_H_INCLUDED
+
+//Bibliotecas utilizadas 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <math.h>
+#include <stdarg.h>  
+#include <stdbool.h>
+
+//Definições de constantes
+#define DISPONIVEL 0
+#define ALUGADO -1
+#define VENDIDO 1
+
+//Definições Básicas e Genéricas das Estruturas
+typedef char NOME[75];
+typedef char CPF[15]; 
+typedef char ENDERECO[100]; 
+typedef char TELEFONE[25]; 
+typedef char MARCA[35]; 
+typedef char MODELO[30]; 
+
+#endif
