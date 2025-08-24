752af0d40eda15b443da657881befc02270c6fe6
Davi Abner <davi.abner57@gmail.com>
2025-08-24 20:54:33 -0300

    feat: adicionar as funcionalidades de funcionarios

---
+++ gerenciamento-concessionaria/entidades/funcionarios.c
@@ -0,0 +1,180 @@
+#include "headers\assinaturas_gerais.h"
+#include "headers\assinaturas_automoveis.h"
+#include "headers\assinaturas_funcionarios.h"
+#include "headers\assinaturas_clientes.h"
+
+//Função para calcular o tamanho de um funcionario
+int tamanho_funcionario(){
+    
+    return ((sizeof(char) * 50) + (sizeof(char) * 15) + (sizeof(char) * 20) + (sizeof(char) * 50) + (sizeof(int)) + (sizeof(double)));
+}
+
+//Função para calcular o tamanho do arquivo de um funcionario
+int tamanho_arquivo_funcionarios(FILE *out){
+    fseek(out, 0, SEEK_END);
+    int tamanho = ftell(out) / sizeof(TFuncionario);
+    return tamanho;
+}   
+
+//Função para ler um funcionário do arquivo
+TFuncionario *leitura_arquivo_funcionario(FILE *in){
+    TFuncionario *func = (TFuncionario *)malloc(sizeof(TFuncionario));
+    if(fread(func, sizeof(TFuncionario), 1 ,in) != 1){
+        free(func);
+        return NULL;
+    }
+
+    return func;
+}
+
+//Função para salvar um funcionário no arquivo
+void salvarFuncionario(TFuncionario *func, FILE *out){
+    fwrite(func, sizeof(TFuncionario), 1, out);
+    fflush(out);
+}
+
+//Função para criar um funcionário manualmente
+TFuncionario *criaFuncionariosManual(TFuncionario *func, FILE *out){
+
+    system("cls");
+
+    if(func == NULL){
+        func = (TFuncionario *)malloc(sizeof(TFuncionario)); 
+        if(func == NULL){
+            printf("Erro ao alocar memoria para cliente.\n");
+            exit(1);
+        }
+    }
+    
+    while(getchar() != '\n'); 
+
+    printf("\nDigite o nome do funcionario: ");
+    fgets(func->nome, sizeof(func->nome), stdin);
+    func->nome[strcspn(func->nome, "\n")] = '\0';
+
+    printf("\nDigite o CPF do funcionario: ");
+    fgets(func->cpf, sizeof(func->cpf), stdin);
+    func->cpf[strcspn(func->cpf, "\n")] = '\0';
+
+    while(getchar() != '\n'){};
+
+    printf("\nDigite o telefone do funcionario: ");
+    fgets(func->telefone, sizeof(func->telefone), stdin);
+    func->telefone[strcspn(func->telefone, "\n")] = '\0';
+
+    printf("\nDigite o endereco do funcionario: ");
+    fgets(func->endereco, sizeof(func->endereco), stdin);
+    func->endereco[strcspn(func->endereco, "\n")] = '\0';
+
+    printf("\nDigite o codigo do funcionario: ");
+    scanf("%d", &func->codigo);
+    getchar(); 
+
+    printf("\nDigite o salario do funcionario: ");
+    scanf("%lf", &func->salario);
+    getchar(); 
+
+    if(out != NULL){
+        fseek(out, 0, SEEK_END);
+        salvarFuncionario(func, out);
+    }
+    
+    return func;
+
+}
+
+//Função para criar um funcionário com parâmetros
+TFuncionario *criaFuncionarios(char *nome, char *cpf, char *telefone, char *endereco, int codigo, double salario){
+
+
+    TFuncionario *func = (TFuncionario*)malloc(sizeof(TFuncionario));
+
+    if(func == NULL){
+
+        printf("\nErro ao alocar memoria para func.\n");
+
+        exit(1);
+    }
+    
+    strcpy(func -> nome, nome);
+    strcpy(func -> cpf, cpf);
+    strcpy(func -> telefone, telefone);
+    strcpy(func -> endereco, endereco);
+
+    func -> codigo = codigo;
+    func -> salario = salario;
+
+    return func;
+}
+
+//Função para criar uma base ordenada de funcionários
+void criarBaseOrdenadaFuncionario(FILE *out, int tamanho){
+
+    int vet[tamanho];
+    TFuncionario *f;
+
+    for(int i=0;i<tamanho;i++)
+        vet[i] = i+1;
+
+    printf("\nGerando a base de dados de funcionarios...");
+
+    for (int i=0;i<tamanho;i++){
+
+        f = criaFuncionarios("Funcionario Ficticio", "000.000.000-00", "00 90000-0000", "Cidade ZZZ - Bairro YYY - Rua XXX - Numero 000", vet[i], 3000*(i+1));
+        salvarFuncionario(f, out);
+    }
+
+    free(f);
+}
+
+//Função para criar uma base embaralhada de funcionários
+void criarBaseEmbaralhadaFuncionario(FILE *out, int tamanho){
+
+
+    int vet[tamanho];
+    TFuncionario *f;
+
+    for(int i=0;i<tamanho;i++)
+        vet[i] = i+1;
+
+
+    embralhabases(vet, tamanho);
+
+    printf("\nGerando a base de dados de funcionarios...");
+
+    for (int i=0;i<tamanho;i++){
+
+        f = criaFuncionarios("Funcionario Ficticio", "000.000.000-00", "00 90000-0000", "Cidade ZZZ - Bairro YYY - Rua XXX - Numero 000", vet[i], 3000*(i+1));
+        salvarFuncionario(f, out);
+    }
+
+    free(f);
+}
+
+//Função para imprimir um funcionário
+void imprimefuncionario(TFuncionario *f) {
+
+    printf("\n| Nome do funcionario: %s\n| CPF: %s\n| Telefone: %s\n", f->nome, f->cpf , f->telefone);
+    printf("| Endereco: %s\n", f->endereco); 
+    printf("| Salario: %.2f\n| Codigo do funcionario: %d\n", f->salario, f->codigo);
+
+    printf("\n\n==============================================================================\n\n");  
+
+}
+
+//Função para imprimir a base de funcionários
+void imprimirBaseFuncionarios(FILE *out){
+
+    system("cls");
+
+    printf("\tDados referentes aos funcionarios\n\n");
+
+    rewind(out);
+
+    TFuncionario *f;
+
+    while((f = leitura_arquivo_funcionario(out)) != NULL){
+        imprimefuncionario(f);
+    }
+    free(f);
+}
--- gerenciamento-concessionaria/headers/assinaturas_funcionarios.h
+++ gerenciamento-concessionaria/headers/assinaturas_funcionarios.h
@@ -14,4 +14,18 @@ typedef struct {
     
 } TFuncionario;
 
+//Declarações de Funções
+TFuncionario *criaFuncionarios(char *nome, char *cpf, char *telefone, char *endereco, int codigo, double salario);
+TFuncionario *criaFuncionariosManual(TFuncionario *func, FILE *out);
+TFuncionario *leitura_arquivo_funcionario(FILE *in);
+
+int tamanho_funcionario();
+int tamanho_arquivo_funcionarios(FILE *out);
+
+void salvarFuncionario(TFuncionario *func, FILE *out);
+void criarBaseOrdenadaFuncionario(FILE *out, int tamanho);
+void criarBaseEmbaralhadaFuncionario(FILE *out, int tamanho);
+void imprimirBaseFuncionarios(FILE *out);
+void imprimefuncionario(TFuncionario *f);
+
 #endif
